47d2e2c894474263421d1f2dabcf76e5730cf94e
Dan <mail.ordan@gmail.com>
2023-10-20 10:31:32 +0300

    Add RandomForestClassifier model generation

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    return "Hello!"
+from sklearn.datasets import make_blobs
+from everywhereml.sklearn.ensemble import RandomForestClassifier
 
+X, y = make_blobs(n_samples=100, centers=3, n_features=2, random_state=0)
+clf = RandomForestClassifier(n_estimators=10)
 
-if __name__ == "__main__":
-    main()
+print(X, y)
+
+clf.fit(X, y)
+
+'''
+  Now we convert the classifier to C++ with a single line of code
+  - instance_name will create an instance of the classifier in the produced code
+    (you will use this name later)
+'''
+clf.to_arduino(instance_name='blobClassifier')
